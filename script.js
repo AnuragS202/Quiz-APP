e051c738c093365c16d9b5f50556efbb1b59262e
Anurag <anurag.singh202@gmail.com>
2021-10-03 23:45:57 +0530

    Some changes in functionality

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var formElement = document.forms['quizForm'];
+let formElement = document.forms['quizApp'];
 
 formElement.onsubmit = function submitAnswers(){
-	var total = 5;
-	var score = 0;
+	let total = 5;
+	let score = 0;
 	
 	// Get User Input
-	var q1 = document.forms["quizForm"]["q1"].value,
-	    q2 = document.forms["quizForm"]["q2"].value,
-	    q3 = document.forms["quizForm"]["q3"].value,
-	    q4 = document.forms["quizForm"]["q4"].value,
-	    q5 = document.forms["quizForm"]["q5"].value;
+	    q1 = document.forms["quizApp"]["q1"].value,
+	    q2 = document.forms["quizApp"]["q2"].value,
+	    q3 = document.forms["quizApp"]["q3"].value,
+	    q4 = document.forms["quizApp"]["q4"].value,
+	    q5 = document.forms["quizApp"]["q5"].value;
 	
 	// Validation
 	for(i = 1; i <= total;i++){
@@ -20,7 +20,7 @@ formElement.onsubmit = function submitAnswers(){
 	}
 	
 	// Set Correct Answers
-	var answers = ["a","b","a","c","a"];
+	let answers = ["a","b","a","c","a"];
 	
 	// Check Answers
 	for(i = 1; i <= total;i++){
@@ -30,8 +30,11 @@ formElement.onsubmit = function submitAnswers(){
 	}
 	
 	// Display Results
-	var results = document.getElementById('results');
+	let results = document.getElementById('results');
 	results.innerHTML = '<h3>You scored <span>'+score+'</span> out of <span>'+total+'</span></h3>';
 	
 	return false;
+	
 }
+
+
